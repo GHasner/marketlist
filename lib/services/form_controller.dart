75e457bdb1fbde7d83691d56e0fa32630422b440
GHasner <140645184+GHasner@users.noreply.github.com>
2024-06-28 16:01:15 -0300

    formatters added

--- lib/services/form_controller.dart
+++ lib/services/form_controller.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:marketlist/models/item.dart';
 
 class FormValidations {
@@ -51,4 +53,55 @@ class FormValidations {
     }
     return true;
   }
+
+  static String formatPrice (double price) {
+    String decimals = price.toString().split('.').last;
+    String integerPart = price.toString().split('.').first;
+    String temp = "";
+    int countThousand = 1;
+    for (int i = integerPart.length - 1; i >= 0; i--) {
+      if (countThousand == 3 && (i - 1) != 0) {
+        temp += ".";
+        countThousand = 0;
+      }
+      temp += integerPart[i];
+      countThousand++;
+    }
+    String formatedPrice = "R\$ ";
+    for (int i = temp.length - 1; i >= 0; i--) {
+      formatedPrice += integerPart[i];
+    }
+    formatedPrice = '$formatedPrice,$decimals';
+    return formatedPrice;
+  }
+
+  static double convertPriceToDouble (String price) {
+    String unformatedPrice = price;
+    unformatedPrice = unformatedPrice.replaceAll("R\$", "");
+    unformatedPrice = unformatedPrice.replaceAll(" ", "");
+    unformatedPrice = unformatedPrice.replaceAll(".", "");
+    double value = double.parse(unformatedPrice);
+    return value;
+  }
+}
+
+class CurrencyInputFormatter extends TextInputFormatter {
+
+    @override
+      TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
+
+        if(newValue.selection.baseOffset == 0){
+            return newValue;
+        }
+
+        double value = double.parse(newValue.text);
+
+        final formatter = NumberFormat.simpleCurrency(locale: "pt_Br");
+
+        String newText = formatter.format(value/100);
+
+        return newValue.copyWith(
+            text: newText,
+            selection: TextSelection.collapsed(offset: newText.length));
+    }
 }
